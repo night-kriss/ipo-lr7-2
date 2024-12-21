0f0b575170e7be52a7b643b260bbfcbaa71a2c48
night-kriss <veseluhadima@gmail.com>
2024-12-21 13:31:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-import json
+filename = 'dump.json'  # Укажите правильный путь к вашему файлу
 
-def load_data(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
+# Загрузка данных из файла
+with open(filename, 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
-def find_qualification(data, qualification_number):
-    results = []
-    for item in data:
-        if item['model'] == 'data.skill' and qualification_number in item['fields']['code']:
-            results.append(item)
-    return results
+# Ввод номера квалификации
+qualification_number = input("Введите номер квалификации: ")
 
-def main():
-    filename = 'dump.json'  # Укажите правильный путь к вашему файлу
-    data = load_data(filename)
-    
-    qualification_number = input("Введите номер квалификации: ")
-    results = find_qualification(data, qualification_number)
-    
-    if results:
-        print("=============== Найдено ===============")
-        for result in results:
-            print(f"{result['fields']['code']} >> {result['fields']['title']}")
-    else:
-        print("=============== Не найдено ===============")
+# Поиск квалификации
+results = []
+for item in data:
+    if item['model'] == 'data.skill' and qualification_number in item['fields']['code']:
+        results.append(item)
 
-if __name__ == "__main__":
-    main()
+# Вывод результатов
+if results:
+    print("=============== Найдено ===============")
+    for result in results:
+        print(f"{result['fields']['code']} >> {result['fields']['title']}")
+else:
+    print("=============== Не найдено ===============")
+
